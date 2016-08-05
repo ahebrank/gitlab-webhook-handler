f944aa18e1b7ecd91f7df8dc078380ef4ee6ff3d
Tomasz Kustrzynski <tomasz.kustrzynski@gmail.com>
2016-08-05 16:25:09 +0100

    Fix empty whitelist case

--- gwh.py
+++ gwh.py
@@ -76,6 +76,7 @@ if __name__ == "__main__":
     port_number = int(args.port)
 
     REPOS_JSON_PATH = args.config
-    WHITELIST_IP = unicode(args.allow, "utf-8")
+    if args.allow:
+        WHITELIST_IP = unicode(args.allow, "utf-8")
 
     app.run(host='0.0.0.0', port=port_number)
