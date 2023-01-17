bed2e7353e5d90989e894e305ac513b7e91ab1df
Andy Hebrank <andy.hebrank@platform.sh>
2023-01-17 16:33:01 -0500

    specify md readme

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     description='Webhook Handler for GitLab',
     license='Apache License, Version 2.0',
     long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
     install_requires=[
         'Flask>=1.0',
         'requests>=2.19.0'
