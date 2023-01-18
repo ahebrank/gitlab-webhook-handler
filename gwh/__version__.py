07e9378c0c50377c45d05239ff46d85c4bb2d718
Andy Hebrank <andy.hebrank@platform.sh>
2023-01-18 15:59:21 -0500

    back to version file

---
+++ gwh/__version__.py
@@ -0,0 +1,3 @@
+__package__ = "gwh"
+# version is injected at build, do not change here
+__version__ = "0.0.0"
--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 import os
 from setuptools import setup
 
+about = {}
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'gwh', '__version__.py'), encoding='utf-8') as f:
+    exec(f.read(), about)
+
 setup(
-    name="gwh",
-    version="develop",
+    name=about['__package__'],
+    version=about['__version__'],
     author='Andy Hebrank',
     author_email='ahebrank@gmail.com',
     packages=['gwh'],
